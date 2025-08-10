425ef58e50f868bf1cda1e828c83570d22e9da3c
Shivam24S <shivamshrivastav8055@gmail.com>
2025-08-10 17:29:52 +0530

    assignment operator

---
+++ lec-2/assignment.c
@@ -0,0 +1,28 @@
+
+
+// assignment operator
+
+// = assign value
+
+// += add and assign
+
+// -= subtract and assign
+
+// *= multiply and assign
+
+// /= divide and assign
+
+// %= modulus and assign
+
+
+
+#include <stdio.h>
+
+int main(){
+
+    int a=5;
+
+    printf("add and asign:%d\n", a+=10); // a = 5 +10 = a =15
+    return 0;
+
+}
