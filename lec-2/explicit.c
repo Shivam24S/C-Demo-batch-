11708b7424b5a49e85d55cbdd6f4ea6eac80e3de
Shivam24S <shivamshrivastav8055@gmail.com>
2025-08-10 17:54:15 +0530

    explicit type conversion

---
+++ lec-2/explicit.c
@@ -0,0 +1,19 @@
+// explicit example
+
+#include <stdio.h>
+
+int main(){
+
+    float pi = 3.14;
+
+    
+
+    printf ("result:%f\n",pi);
+
+    int intPI = (int)pi;
+
+    printf("result:%d\n",intPI);
+
+    return 0;
+
+}
