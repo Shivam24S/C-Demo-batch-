caeb2cb2ecf254391764087791cbc5cd1cb29c2c
Shivam24S <shivamshrivastav8055@gmail.com>
2025-08-10 17:45:42 +0530

    implicit type conversion

---
+++ lec-2/typeconversion.c
@@ -0,0 +1,33 @@
+
+
+// type conversion means converting our datatypes into other datatypes
+
+// two types of conversion
+
+// 1.implicit --> automatic by compiler
+
+// 2. explicit --> forcefully by us 
+
+// implicit example
+
+
+#include <stdio.h>
+
+int main(){
+
+    int a = 10;
+
+    float b =15.7;
+
+    float result = a+b;
+
+
+    printf("result:%f\n",(result));
+
+    return 0;
+
+
+}
+
+
+
