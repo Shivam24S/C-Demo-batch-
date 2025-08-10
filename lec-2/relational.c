aa0dbf194e72b789f8dea42f34b250fe7318e9fc
Shivam24S <shivamshrivastav8055@gmail.com>
2025-08-10 17:03:57 +0530

    relational operator

---
+++ lec-2/relational.c
@@ -0,0 +1,22 @@
+
+// relational == comparison operator
+
+// <, <=, >, >=, ==, !=
+
+// return  for true and 0 for false 
+
+
+#include <stdio.h>
+
+int main(){
+
+    int a = 10, b =7;
+
+
+    printf("< lessthan OP:%d\n",a<7);
+
+    return 0;
+
+
+}
+
