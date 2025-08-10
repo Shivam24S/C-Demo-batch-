301baa9df18d14133d7eaf1751a7bb31744dfa58
Shivam24S <shivamshrivastav8055@gmail.com>
2025-08-10 18:06:36 +0530

    (x+y)*2 formula example added

---
+++ lab-2_1/math_swap_program1.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main(){
+
+    float x,y,result;
+
+    printf("enter the value of x:\n");
+
+    scanf("%f",&x);
+
+     printf("enter the value of y:\n");
+    
+        scanf("%f",&y);
+        
+
+        result = (x + y) + (x+y);
+
+        printf("result:%f\n",result);
+
+        return 0;
+
+}
