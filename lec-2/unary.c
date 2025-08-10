57954cad9a16266b54915ed1722928c6285200fe
Shivam24S <shivamshrivastav8055@gmail.com>
2025-08-10 16:46:51 +0530

    unary operator

---
+++ lec-2/unary.c
@@ -0,0 +1,23 @@
+
+// unary operator means we will perform operation on single operands
+
+// +
+// -
+// ++ can be prefix and postfix
+// --
+
+
+#include <stdio.h>
+
+int main(){
+
+    int x =5;
+
+    printf("unary + :%d\n", +x); // no effect 
+
+
+        printf("unary + :%d\n", x++);
+
+
+
+}
